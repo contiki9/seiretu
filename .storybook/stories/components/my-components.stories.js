c571263523be296e31e3aaf5d104adc137721905
contiki9 <contiki9@gmail.com>
2020-12-20 12:24:25 +0900

    :+1: storybook -> addon

---
+++ .storybook/stories/components/my-components.stories.js
@@ -0,0 +1,58 @@
+import { storiesOf } from '@storybook/html';
+import { withKnobs, select,boolean,text } from '@storybook/addon-knobs';
+import { withActions } from '@storybook/addon-actions';
+
+
+storiesOf('components', module)
+  .addDecorator(withActions('onClick'))
+  .addDecorator(withKnobs)
+  .add('row', () => {
+    const group = 'layout'
+    const alignOptions = {
+      stretch: 'stretch',
+      start: 'start',
+      center: 'center',
+      end: 'end',
+    };
+    const justifyOptions = {
+      stretch: 'stretch',
+      start: 'start',
+      center: 'center',
+      end: 'end',
+      spaceBetween: 'space-between',
+      spaceAround: 'space-around',
+      spaceEvenly: 'space-evenly',
+    };
+    const alignContentOptions = {
+      stretch: 'stretch',
+      start: 'start',
+      center: 'center',
+      end: 'end',
+      spaceBetween: 'space-between',
+      spaceAround: 'space-around',
+      spaceEvenly: 'space-evenly',
+    };
+
+    const justify = select('justify', justifyOptions, null, group);
+    const alignContent = select('alignContent', alignContentOptions, null, group);
+    const align = select('align', alignOptions, null, group);
+    const space = text('space', '40px',group);
+    const nowrap = boolean('nowrap', false, group);
+    const column = boolean('column', false, group);
+    return(`
+      <srt-row
+       justify = ${justify}
+       align = ${align}
+       nowrap = ${nowrap}
+       space = ${space}
+       column = ${column}
+       >
+            <div>sample
+              <div>ssss</div>
+            </div>
+              <div>sample2</div>
+              <div>sample3</div>
+      </srt-row>`
+    )
+    }
+  )
---
+++ src/components/srt-row/readme.md
@@ -0,0 +1,22 @@
+# srt-row
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property       | Attribute       | Description | Type                                                                                               | Default     |
+| -------------- | --------------- | ----------- | -------------------------------------------------------------------------------------------------- | ----------- |
+| `align`        | `align`         |             | `"center" \| "end" \| "start" \| "stretch"`                                                        | `undefined` |
+| `alignContent` | `align-content` |             | `"center" \| "end" \| "space-around" \| "space-between" \| "space-evenly" \| "start" \| "stretch"` | `undefined` |
+| `column`       | `column`        |             | `boolean`                                                                                          | `undefined` |
+| `justify`      | `justify`       |             | `"center" \| "end" \| "space-around" \| "space-between" \| "space-evenly" \| "start" \| "stretch"` | `undefined` |
+| `nowrap`       | `nowrap`        |             | `boolean`                                                                                          | `undefined` |
+| `space`        | `space`         |             | `string`                                                                                           | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
